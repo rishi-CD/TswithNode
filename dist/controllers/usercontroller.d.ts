ab42e5ffc57aaa4d52a40e1da1b06bd36c5f2f8c
rishi-cd <rishibala.kanthavel@crystaldelta.com>
2025-08-05 16:18:03 +0530

    updated

--- dist/controllers/usercontroller.d.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express';
-import { User, CreateUserDTO } from '../types/userType';
-export declare const getUsers: (req: Request, res: Response<User[]>) => void;
-export declare const createUser: (req: Request<{}, {}, CreateUserDTO>, res: Response<User | {
-    error: string;
-}>) => Response<User | {
-    error: string;
-}, Record<string, any>> | undefined;
-//# sourceMappingURL=usercontroller.d.ts.map
--- dist/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const usercontroller_1 = require("../controllers/usercontroller");
-const router = (0, express_1.Router)();
-router.get('/', usercontroller_1.getUsers);
-router.post('/', usercontroller_1.createUser);
-exports.default = router;
-//# sourceMappingURL=userRoutes.js.map
